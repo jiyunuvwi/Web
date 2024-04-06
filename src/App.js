e97904fca98cbcbc99f4111b89246a1866af2780
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-04-06 19:08:59 +0900

    MOD : Router main 밖으로

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
 import {Route, Routes} from 'react-router-dom';
 
 import Home from "./pages/Home";
-import Sidebar from "./components/sidebar";
-import Topnav from "./components/topnav";
 
 
 export default function App() {
 
-    const [toggle, setToggle] = useState(true);
-
   return (
       <>
-          <div className={`page-wrapper bg-black ${toggle ? "toggled" : ""}`}>
-              <Sidebar/>
-              <main className="page-content h-screen">
-                  <Topnav toggle={toggle} setToggle={setToggle}/>
-
-                  <Routes>
-                      <Route path="/home" element={<Home />} />
-                  </Routes>
-              </main>
-          </div>
+          <Routes>
+              <Route path="/home" element={<Home />} />
+          </Routes>
       </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
+import Sidebar from "../components/sidebar";
+import Topnav from "../components/topnav";
 
 export default function Home(){
 
+    const [toggle, setToggle] = useState(true);
+
     return(
         <>
+            <div className={`page-wrapper bg-black ${toggle ? "toggled" : ""}`}>
+                <Sidebar/>
+                <main className="page-content h-screen">
+                    <Topnav toggle={toggle} setToggle={setToggle}/>
+
+                    <div className="relative top-25 mx-10">
+                        <h3 className="text-4xl text-white font-bold">DashBoard</h3>
+                    </div>
+                </main>
+            </div>
         </>
     )
 }

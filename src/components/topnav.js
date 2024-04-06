cc7b446916d438576cef32dec717032a016356b1
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-04-06 19:08:25 +0900

    MOD : 드롭다운 색상 변경

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -42,13 +42,13 @@ export default function Topnav({setToggle, toggle}){
                                 <span className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[20px] text-center bg-gray-50 hover:bg-gray-100 border border-gray-100 text-slate-900 rounded-full"><FaUser className="icon" /></span>
                                 <span className="font-semibold text-[16px] text-lg text-white ms-1 px-2 sm:inline-block">관리자님</span>
                             </button>
-                            <div className={`dropdown-menu absolute end-0 m-0 mt-4 z-10 w-44 rounded-md overflow-hidden bg-white shadow ${user ? '' : 'hidden'}`} >
+                            <div className={`dropdown-menu absolute end-0 m-0 mt-4 z-10 w-44 rounded-md overflow-hidden bg-black border border-gray-400 shadow ${user ? '' : 'hidden'}`} >
                                 <ul className="py-2 text-start">
                                     <li>
-                                        <Link to="/setting" className="flex items-center font-medium py-1 px-4 hover:text-indigo-600 "><AiOutlineSetting className="me-2"/>관리자 설정</Link>
+                                        <Link to="/setting" className="flex items-center font-medium py-1 px-4 text-white hover:text-yellow-500"><AiOutlineSetting className="me-2"/>관리자 설정</Link>
                                     </li>
                                     <li>
-                                        <Link to="/login" className="flex items-center font-medium py-1 px-4 hover:text-indigo-600"><IoMdLogOut className="me-2"/>로그아웃</Link>
+                                        <Link to="/login" className="flex items-center font-medium py-1 px-4 text-white hover:text-yellow-500"><IoMdLogOut className="me-2"/>로그아웃</Link>
                                     </li>
                                 </ul>
                             </div>
